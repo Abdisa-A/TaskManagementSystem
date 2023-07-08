f3f3dc0e929a5b422671d433942b7f08ce14b2d4
Abdisa Alemu <abdisa.alemu@aait.edu.et>
2023-07-08 22:46:33 +0300

    text box ui on todos

--- script/main.js
+++ script/main.js
@@ -1,19 +1,11 @@
-const inputTag = document.createElement("input")
-const inputDiv = document.querySelector(".input-div")
+
+const inputTask = document.querySelector(".input-task")
 const addTaskButton = document.querySelector(".add-btn")
 const addBtn = document.createElement("i")
 const delBtn = document.createElement("i")
 
 
-addBtn.setAttribute("class", "fa-solid fa-plus")
-delBtn.setAttribute("class", "fa-solid fa-trash")
-
-addBtn.style = "color: red, margin-left: 20px"
-
 addTaskButton.addEventListener("click", (e) => {
-    e.preventDefault()
-inputDiv.append(inputTag)
-inputDiv.append(addBtn)
-inputDiv.append(delBtn)
+    
 })
 
