280b3394acb070236ee3317110b8df8930312a12
Abdisa Alemu <abdisa.alemu@aait.edu.et>
2023-07-09 01:23:24 +0300

    add task functionality

--- script/main.js
+++ script/main.js
@@ -1,11 +1,28 @@
-
+const inputDiv = document.querySelector(".input-div")
 const inputTask = document.querySelector(".input-task")
 const addTaskButton = document.querySelector(".add-btn")
-const addBtn = document.createElement("i")
-const delBtn = document.createElement("i")
+const addIcon = document.querySelector(".add-icon")
+const  todoList = document.querySelector(".todos")
+let todoValue = [];
 
 
+let displayChecker = true
 addTaskButton.addEventListener("click", (e) => {
+    e.preventDefault()
+    inputDiv.style = displayChecker ? "display: block" : "display: none" 
+   
+})
+
+
+inputTask.addEventListener("change", (event) => {
+    let {value} = event.target
+    // todoValue = value
+    todoValue.push({value: value, date: new Date().getDay()})
+    value = ""
+})
+addIcon.addEventListener("click", () => {
+    todoList.append(todoValue)
     
 })
 
+console.log(todoValue)
